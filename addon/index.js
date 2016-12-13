ea88255526ff48fac557c4a510bde63203d95202
Paul Chavard <git@paul.chavard.net>
2016-12-13 20:03:05 +0100

    use PouchDB.defaults

--- addon/index.js
+++ addon/index.js
@@ -1,22 +1,7 @@
-
-export default class PouchDB extends BaseClass() {
-  constructor(name, options = {}) {
-    if (isFastBoot && options.adapter === 'memory') {
-      options.db = FastBoot.require('memdown');
-      delete options.adapter;
-    }
-
-    super(name, options);
-  }
-}
-
 const isFastBoot = typeof FastBoot !== 'undefined';
 
-function BaseClass() {
-  if (isFastBoot) {
-    return FastBoot.require('pouchdb')
-      .plugin(FastBoot.require('pouchdb-find'));
-  }
+const PouchDB = isFastBoot ? FastBoot.require('pouchdb')
+  .plugin(FastBoot.require('pouchdb-find'))
+  .defaults({ db: FastBoot.require('memdown') }) : self.PouchDB;
 
-  return self.PouchDB;
-}
+export default PouchDB;
